022528dbd7a199d340bd5cff93708668ae44ba29
IvanBoiko92 <106371880+IvanBoiko92@users.noreply.github.com>
2022-09-28 10:34:34 +0300

    Add files via upload

---
+++ src/main/java/com/epam/rd/autotasks/meetautocode/ElectronicWatch.java
@@ -0,0 +1,17 @@
+package com.epam.rd.autotasks.meetautocode;
+
+import java.util.Scanner;
+
+public class ElectronicWatch {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int seconds = scanner.nextInt();
+        int h = (seconds / 3600)%24;
+        int m = (seconds % 3600) / 60;
+        int s = seconds % 60;
+ 
+        System.out.printf("%d:%02d:%02d", h, m, s);
+
+
+    }
+}
